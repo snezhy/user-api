d2534899281ccfb24d09a8e778bfac389b2794a3
Snezhana <snezhy@rocketmail.com>
2018-11-12 06:55:37 +0000

    Adding PATCH /users/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectId} = require('mongodb'); 
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb'); 
 
 let {mongoose} = require('./db/mongoose');
 let {User} = require('./models/user');
@@ -69,6 +70,27 @@ app.delete('/users/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.patch('/users/:id', (req, res) => {
+    let id = req.params.id;
+
+    let body = _.pick(req.body, ['email', 'firstName', 'lastName', 'address']);
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send("Id is invalid");
+    }
+
+    User.findOneAndUpdate(id, {$set: body}, {new: true}).then((user) => {
+        if (!user) {
+          return res.status(404).send();
+        }
+    
+        res.send({user});
+      }).catch ((e) => {
+        res.status(400).send();
+    })
+
+} )
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
